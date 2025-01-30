2b2cbf20153a83200df5b2576513d7061ccba11c
Babajii91 <jrakotonirina91@gmail.com>
2025-01-30 19:04:43 +0100

    modified:   ../java/fr/efrei/pokemon_tcg/repositories/AttaqueRepository.java

--- src/main/java/fr/efrei/pokemon_tcg/repositories/AttaqueRepository.java
+++ src/main/java/fr/efrei/pokemon_tcg/repositories/AttaqueRepository.java
@@ -1,7 +1,6 @@
 package fr.efrei.pokemon_tcg.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import fr.efrei.pokemon_tcg.models.Attaque;
 
 public interface AttaqueRepository extends JpaRepository <Attaque, String> {
 	
